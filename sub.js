50d168347025b56abe38b4ce1ab0fede997a8380
minkwonji <89757007+minkwonji@users.noreply.github.com>
2021-08-30 11:58:11 +0900

    Initial commit

---
+++ sub.js
@@ -0,0 +1,6 @@
+    document.getElementById("modal_open_btn").onclick = function() {
+        document.getElementById("modal").style.display="block";
+    }    
+    document.getElementById("modal_close_btn").onclick = function() {
+        document.getElementById("modal").style.display="none";
+    }   
